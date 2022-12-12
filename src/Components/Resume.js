4775fa3e2c992171f470255576230e9f78a48f01
Miles Raker <milesraker@gmail.com>
2022-12-11 20:36:19 -0800

    updated values

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -96,21 +96,6 @@ const leftColumnStyle = {
     backgroundColor: customThemeOne.palette.primary.light
 }
 
-const styles = theme => ({
-    '@global': {
-        '*::-webkit-scrollbar': {
-            width: '0.4em'
-        },
-        '*::-webkit-scrollbar-track': {
-            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
-        },
-        '*::-webkit-scrollbar-thumb': {
-            backgroundColor: 'rgba(0,0,0,.1)',
-            outline: '1px solid slategrey'
-        }
-    }
-});
-
 
 /** Render a single skill List Item
  * @param skill a string
